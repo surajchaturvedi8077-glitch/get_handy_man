e7d863a33bf68317ce5daafe9604fcdf3fbc8cbe
suraj <rishabh@mematdigi.com>
2026-09-24 18:00:53 +0530

    latest changes

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,14 @@
 const app = require('./src/app');
 const connectDB = require('./src/config/db');
 const { port } = require('./src/config/env');
-const { startCronJobs } = require('./src/services/cronService');
+// const { startCronJobs } = require('./src/services/cronService');
 
 // 1. Connect to DB in the background
 connectDB().catch(err => {
   console.error('[server] MongoDB connection error:', err);
 });
 
-startCronJobs();
+// startCronJobs();
 // 2. Start listening IMMEDIATELY so Hostinger is happy
 app.listen(port, () => {
   // eslint-disable-next-line no-console
